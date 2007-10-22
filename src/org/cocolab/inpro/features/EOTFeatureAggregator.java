c0a8c7f446ee6c181ed1680e3ffb450e605c5216
Timo Baumann <baumann@informatik.uni-hamburg.de>
2007-10-22 22:21:45 +0000

    change time from seconds to frames; add features for regression of logEnergy
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@56 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/features/EOTFeatureAggregator.java
+++ src/org/cocolab/inpro/features/EOTFeatureAggregator.java
@@ -9,12 +9,21 @@ import edu.cmu.sphinx.util.props.Resetable;
 
 public class EOTFeatureAggregator implements Resetable {
 	
-	private final static int NUMBER_OF_ATTRIBUTES = 5; 
+	private final static int NUMBER_OF_ATTRIBUTES = 14; 
 	
 	private Attribute timeIntoTurnAttribute; 
 	private Attribute wordsIntoTurnAttribute;
 	private Attribute timeIntoLastWordAttribute;
 	private Attribute currentFrameEnergyAttribute;
+	private Attribute frameEnergy50msMeanAttribute;
+	private Attribute frameEnergy50msSlopeAttribute;
+	private Attribute frameEnergy50msPredictAttribute;
+	private Attribute frameEnergy100msMeanAttribute;
+	private Attribute frameEnergy100msSlopeAttribute;
+	private Attribute frameEnergy100msPredictAttribute;
+	private Attribute frameEnergy200msMeanAttribute;
+	private Attribute frameEnergy200msSlopeAttribute;
+	private Attribute frameEnergy200msPredictAttribute;
 	
 	protected final static boolean CLUSTERED_TIME = true; // FIXME: this should be configurable (maybe via constructor) 
 	
@@ -23,10 +32,13 @@ public class EOTFeatureAggregator implements Resetable {
 	
 	protected Instances instances;
 	
-	double timeIntoTurnValue;
+	int framesIntoTurnValue;
 	int wordsIntoTurnValue;
-	double timeIntoLastWordValue;
+	int framesIntoLastWordValue;
 	double currentFrameEnergyValue;
+	TimeShiftingAnalysis frameEnergy50ms = new TimeShiftingAnalysis(5);
+	TimeShiftingAnalysis frameEnergy100ms = new TimeShiftingAnalysis(10);
+	TimeShiftingAnalysis frameEnergy200ms = new TimeShiftingAnalysis(20); 
 	
 	
 	public EOTFeatureAggregator() {
@@ -40,7 +52,27 @@ public class EOTFeatureAggregator implements Resetable {
 		currentFrameEnergyAttribute = new Attribute("currentFrameEnergy");
 		attInfo.addElement(currentFrameEnergyAttribute);
 
-		
+		frameEnergy50msMeanAttribute = new Attribute("frameEnergy50msMean");
+		attInfo.addElement(frameEnergy50msMeanAttribute);
+		frameEnergy50msSlopeAttribute = new Attribute("frameEnergy50msSlope");
+		attInfo.addElement(frameEnergy50msSlopeAttribute);
+		frameEnergy50msPredictAttribute = new Attribute("frameEnergy50msPredict");
+		attInfo.addElement(frameEnergy50msPredictAttribute);
+
+		frameEnergy100msMeanAttribute = new Attribute("frameEnergy100msMean");
+		attInfo.addElement(frameEnergy100msMeanAttribute);
+		frameEnergy100msSlopeAttribute = new Attribute("frameEnergy100msSlope");
+		attInfo.addElement(frameEnergy100msSlopeAttribute);
+		frameEnergy100msPredictAttribute = new Attribute("frameEnergy100msPredict");
+		attInfo.addElement(frameEnergy100msPredictAttribute);
+
+		frameEnergy200msMeanAttribute = new Attribute("frameEnergy200msMean");
+		attInfo.addElement(frameEnergy200msMeanAttribute);
+		frameEnergy200msSlopeAttribute = new Attribute("frameEnergy200msSlope");
+		attInfo.addElement(frameEnergy200msSlopeAttribute);
+		frameEnergy200msPredictAttribute = new Attribute("frameEnergy200msPredict");
+		attInfo.addElement(frameEnergy200msPredictAttribute);
+
 		if (CLUSTERED_TIME) {
 			clusteredTimeToEOT = EOTBins.eotBinsAttribute();
 			attInfo.addElement(clusteredTimeToEOT);
@@ -61,39 +93,70 @@ public class EOTFeatureAggregator implements Resetable {
 	
 	protected Instance getNewestFeatures() {
 		Instance instance = new Instance(NUMBER_OF_ATTRIBUTES);
-		instance.setValue(timeIntoTurnAttribute, timeIntoTurnValue);
+		instance.setValue(timeIntoTurnAttribute, ((double) framesIntoTurnValue) / 100.0);
 		instance.setValue(wordsIntoTurnAttribute, wordsIntoTurnValue);
-		instance.setValue(timeIntoLastWordAttribute, timeIntoLastWordValue);
+		instance.setValue(timeIntoLastWordAttribute, ((double) framesIntoLastWordValue) / 100.0);
 		instance.setValue(currentFrameEnergyAttribute, currentFrameEnergyValue);
+		instance.setValue(frameEnergy50msMeanAttribute, 
+						  frameEnergy50ms.getMean());
+		instance.setValue(frameEnergy50msSlopeAttribute, 
+						  frameEnergy50ms.getSlope());
+		instance.setValue(frameEnergy50msPredictAttribute, 
+						  frameEnergy50ms.predictValueAt(framesIntoTurnValue));
+		
+		instance.setValue(frameEnergy100msMeanAttribute, 
+				  		  frameEnergy100ms.getMean());
+		instance.setValue(frameEnergy100msSlopeAttribute, 
+						  frameEnergy100ms.getSlope());
+		instance.setValue(frameEnergy100msPredictAttribute, 
+				  		  frameEnergy100ms.predictValueAt(framesIntoTurnValue));
+		
+		instance.setValue(frameEnergy200msMeanAttribute, 
+						  frameEnergy200ms.getMean());
+		instance.setValue(frameEnergy200msSlopeAttribute, 
+						  frameEnergy200ms.getSlope());
+		instance.setValue(frameEnergy200msPredictAttribute, 
+						  frameEnergy200ms.predictValueAt(framesIntoTurnValue));
+
 		instance.setDataset(instances);
 		instance.setClassMissing();
 		return instance;
 	}
 	
-	public void setTimeIntoTurn(double t) {
-		timeIntoTurnValue = t;
+	public void setFramesIntoTurn(int f) {
+		framesIntoTurnValue = f;
+	}
+	
+	public double getFramesIntoTurn() {
+		return framesIntoTurnValue;
 	}
 
 	public double getTimeIntoTurn() {
-		return timeIntoTurnValue;
+		return ((double) framesIntoTurnValue) / 100.0; // FIXME: this should be configurable 
 	}
 	
 	public void setWordsIntoTurn(int wordsIntoTurn) {
 		wordsIntoTurnValue = wordsIntoTurn;
 	}
 	
-	public void setTimeIntoLastWord(double timeIntoLastWord) {
-		timeIntoLastWordValue = timeIntoLastWord;
+	public void setFramesIntoLastWord(int framesIntoLastWord) {
+		framesIntoLastWordValue = framesIntoLastWord;
 	}
 	
 	public void setCurrentFrameEnergy(double logEnergy) {
 		currentFrameEnergyValue = logEnergy;
+		frameEnergy50ms.add(framesIntoTurnValue, logEnergy);
+		frameEnergy100ms.add(framesIntoTurnValue, logEnergy);
+		frameEnergy200ms.add(framesIntoTurnValue, logEnergy);
 	}
 
 	public void reset() {
-		timeIntoTurnValue = -1.0;
+		framesIntoTurnValue = -1;
 		wordsIntoTurnValue = -1;
-		timeIntoLastWordValue = -1.0;
+		framesIntoLastWordValue = -1;
 		currentFrameEnergyValue = -1.0;
+		frameEnergy50ms.reset();
+		frameEnergy100ms.reset();
+		frameEnergy200ms.reset();
 	}
 }
