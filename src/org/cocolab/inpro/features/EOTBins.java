cd74aa89ca20a1ed373a2a0baef39473834f003d
Timo Baumann <baumann@informatik.uni-hamburg.de>
2007-10-31 21:35:24 +0000

    simpler bins
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@74 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/features/EOTBins.java
+++ src/org/cocolab/inpro/features/EOTBins.java
@@ -5,7 +5,7 @@ import weka.core.FastVector;
 
 public class EOTBins {
 
-	public final static String[] EOT_BINS = {
+/*	public final static String[] EOT_BINS = {
 		"moreThan2560ms", "2560to1281ms", "1280to641ms", "640to321ms", "320to161ms",
 		"160to81ms", "80to41ms", "40to1ms", "0to39msAgo", "40to79msAgo", "80to159msAgo",
 		"moreThan160msAgo"
@@ -14,6 +14,13 @@ public class EOTBins {
 	private final static double[] EOT_BIN_ENDS = {
 		2.561, 1.281, 0.641, 0.321, 0.161, 0.081, 0.041, 0.001, -0.039, -0.079, -0.159, -Double.MAX_VALUE
 	};
+*/
+
+	public final static String[] EOT_BINS = {
+		"moreThan640ms", "640to161ms", "161to81ms", "80to1ms", "0to38msAgo", "40to119msAgo", "moreThan120msAgo" 
+	};
+	
+	private final static double[] EOT_BIN_ENDS = {0.641, 0.161, 0.081, 0.001, -0.039, -0.119, -Double.MAX_VALUE}; 
 	
 	public static Attribute eotBinsAttribute() {
 		FastVector binnedTimesToEOT = new FastVector(EOT_BINS.length);
