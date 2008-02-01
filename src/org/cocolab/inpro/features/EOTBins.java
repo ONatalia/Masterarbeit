3fd7819d39061a1aec24b8ff4c2976c85551d25c
Timo Baumann <baumann@informatik.uni-hamburg.de>
2008-02-01 09:15:44 +0000

    do complete speech binning instead of only EOT: (not only TURN, EOT, SIL, but also have classes for start of turn, e.g.: SIL, SOT, TURN, EOT, SIL)
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@249 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/features/EOTBins.java
+++ src/org/cocolab/inpro/features/EOTBins.java
@@ -3,7 +3,108 @@ package org.cocolab.inpro.features;
 import weka.core.Attribute;
 import weka.core.FastVector;
 
+/**
+ * class for binning time to turn-relevant units 
+ * @author timo
+ *
+ * 
+ * the turn-relevant units can be defined relative to start-of-turn (SOT)
+ * and end-of-turn (EOT).
+ *
+ * for backwards compatibility, units can be measured in distance to eot alone
+ * 
+ * 
+ */
+
 public class EOTBins {
+	
+	public final static String[] SOT_BINS = {
+		"sil"
+	};
+	
+	private final static double[] SOT_BIN_ENDS = {
+		0.0
+	};
+	
+	public final static String[] EOT_BINS = {
+		"talk", "EOT", "sil"
+	};
+	
+	private final static double[] EOT_BIN_ENDS = {
+		0.51, -0.050, -Double.MAX_VALUE
+	};
+	
+	/*  
+	 * return a weka attribute with the values for eot-bins
+	 */ 
+	public static Attribute eotBinsAttribute() {
+		FastVector binnedTimesToEOT = new FastVector(EOT_BINS.length);
+		for (int i = 0; i < EOT_BINS.length; i++) {
+			binnedTimesToEOT.addElement(EOT_BINS[i]);
+		}
+		Attribute a = new Attribute("binnedTimeToEOT", binnedTimesToEOT);
+		return a;
+	}
+	
+	/*
+	 * return a weka attribute with values for all turn-bins (EOT_BINS u SOT_BINS)
+	 */
+	public static Attribute turnBinsAttribute() {
+		FastVector turnBins = new FastVector(SOT_BINS.length + EOT_BINS.length);
+		for (int i = 0; i < SOT_BINS.length; i++) {
+			turnBins.addElement(SOT_BINS[i]);
+		}
+		for (int i = 0; i < EOT_BINS.length; i++) {
+			if (!turnBins.contains(EOT_BINS[i])) {
+				turnBins.addElement(EOT_BINS[i]);
+			}
+		}
+		Attribute a = new Attribute("turnBins", turnBins);
+		return a;		
+	}
+	
+	/**
+	 * finds the right bin for the given distance to EOT
+	 * @return bin-name
+	 */
+	public static String eotBin(double eot) {
+		for (int i = 0; i < EOT_BINS.length; i++) {
+			if (eot > EOT_BIN_ENDS[i]) {
+				return EOT_BINS[i];
+			}
+		}
+		// this should never be reached
+		assert false;
+		return null;
+	}
+	
+	/**
+	 * finds the right bin for the given distances to SOT and EOT
+	 * @param sot distance to start of turn
+	 * @param eot dist. to eot
+	 * @return bin-name
+	 */
+	public static String turnState(double sot, double eot) {
+		// check if we are in the beginning of the turn and return the corresponding bin
+		for (int i = 0; i < SOT_BINS.length; i++) {
+			if (sot < SOT_BIN_ENDS[i]) {
+				return SOT_BINS[i];
+			}
+		}
+		// otherwise return bin relative to eot
+		return eotBin(eot);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Testing class EOTBins");
+		for (double d = 2.6; d > -0.5; d -= 0.02) {
+			System.out.println(d + " is " + eotBin(d));
+		}
+	}
+	
+}
+
+
 
 /*	public final static String[] EOT_BINS = {
 		"moreThan2560ms", "2560to1281ms", "1280to641ms", "640to321ms", "320to161ms",
@@ -34,39 +135,3 @@ public class EOTBins {
 		0.101, -Double.MAX_VALUE
 	};
 */ 
-	public final static String[] EOT_BINS = {
-		"notYet", "soon", "now"
-	};
-	
-	private final static double[] EOT_BIN_ENDS = {
-		0.101, -0.050, -Double.MAX_VALUE
-	};
-	
-	public static Attribute eotBinsAttribute() {
-		FastVector binnedTimesToEOT = new FastVector(EOT_BINS.length);
-		for (int i = 0; i < EOT_BINS.length; i++) {
-			binnedTimesToEOT.addElement(EOT_BINS[i]);
-		}
-		Attribute a = new Attribute("binnedTimeToEOT", binnedTimesToEOT);
-		return a;
-	}
-	
-	public static String eotBin(double eot) {
-		for (int i = 0; i < EOT_BINS.length; i++) {
-			if (eot > EOT_BIN_ENDS[i]) {
-				return EOT_BINS[i];
-			}
-		}
-		// this should never be reached
-		assert false;
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Testing class EOTBins");
-		for (double d = 2.6; d > -0.5; d -= 0.02) {
-			System.out.println(d + " is " + eotBin(d));
-		}
-	}
-	
-}
--- src/org/cocolab/inpro/features/EOTTrainer.java
+++ src/org/cocolab/inpro/features/EOTTrainer.java
@@ -1,42 +1,72 @@
 package org.cocolab.inpro.features;
 
 import java.io.IOException;
+import java.util.List;
 
 import weka.core.Instance;
 
 public class EOTTrainer extends EOTFeatureAggregator {
 
-	private double EOT;
+	private double startTime;
+	private double stopTime;
 
 	public EOTTrainer() {
 		System.out.println(instances);
 	}
 
+	protected Instance getNewestFeatures() {
+		Instance instance = super.getNewestFeatures();
+		double timeIntoTurn = getTimeIntoTurn();
+		double remainingTime = stopTime - getTimeIntoAudio();
+		if ((timeIntoTurn >= 0.f) && (!EOTBins.turnState(timeIntoTurn, remainingTime).equals("sil"))) {
+			instance.setValue(framesIntoTurnAttribute, timeIntoTurn);
+		} else {
+			instance.setValue(framesIntoTurnAttribute, 0.f);
+		}
+		return instance;
+	}
+	
 	public void loadGoldStandard(String filename) {
 		if (filename != null) {
 			try {
-				filename = filename.replaceAll("\\.\\w+$", ".ortho");
+				filename = filename.replaceAll("\\.\\w+$", ".lab");
 //				filename = filename.replaceAll("/data/", "/par/");
-				filename = filename.replaceAll("/data/", "/annot/ortho/");
+//				filename = filename.replaceAll("/data/", "/annot/ortho/");
 				filename = filename.replaceAll("file:", "");
-				String labelLine = LabelFile.getLastLine(filename);
-				EOT = LabelFile.getStopTime(labelLine);
+				List<String> labelLines = LabelFile.getLines(filename, 0);
+				startTime = Math.floor(LabelFile.getStartTime(labelLines.get(1)) * 100) / 100;
+				stopTime =  Math.floor(LabelFile.getStopTime(labelLines.get(labelLines.size() - 1)) * 100) / 100;
+				System.err.println("EOTTrainer: start: " + startTime + ", stop: " + stopTime);
 			} catch (IOException e) {
-				EOT = 0.f;
+				startTime = 0.f;
+				stopTime = 0.f;
 				e.printStackTrace();
 			}
 		}
 	}
 	
+	public double getSOTDistance() {
+		return startTime - ((double) framesIntoAudioValue) / 100.0f;
+	}
+	
 	public double getEOTDistance() {
-		return EOT - ((double) framesIntoTurnValue) / 100.0f;
+		return stopTime - ((double) framesIntoAudioValue) / 100.0f;
+	}
+	
+	/*
+	 * if in turn: time since turn beginning
+	 * otherwise: 0.0
+	 */
+	public double getTimeIntoTurn() {
+		return getTimeIntoAudio() - startTime;
 	}
 
 	public Instance getCurrentInstance() {
 		Instance inst = getNewestFeatures();
-		double remainingTime = EOT - getTimeIntoTurn();
+		double timeIntoTurn = getTimeIntoTurn();
+		double remainingTime = stopTime - getTimeIntoAudio();
 		if (CLUSTERED_TIME) {
-			inst.setValue(clusteredTimeToEOT, EOTBins.eotBin(remainingTime));
+			inst.setValue(clusteredTimeToEOT, EOTBins.turnState(timeIntoTurn, remainingTime));
 		}
 		if (CONTINUOUS_TIME) {
 			inst.setValue(timeToEOT, remainingTime);
