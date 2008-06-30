2c4be1e5d44976eca8f91e1062991f24558de9d4
Timo Baumann <baumann@informatik.uni-hamburg.de>
2008-06-30 23:04:23 +0000

    code cleanup
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@446 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/deawu/pentomino/Grid.java
+++ src/org/cocolab/deawu/pentomino/Grid.java
@@ -6,8 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.text.CharacterIterator;
-import java.text.StringCharacterIterator;
 
 class Grid {
 	
