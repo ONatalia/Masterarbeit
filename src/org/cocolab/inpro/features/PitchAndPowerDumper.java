c578dfc9c5498ddfa186c142432c8cff67cf72dd
Timo Baumann <baumann@informatik.uni-hamburg.de>
2008-07-21 18:20:58 +0000

    nice new feature to dump base features (pitch and power) directly without creating derived features
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@480 19460358-522e-0410-942e-fdb976ae3ca2

---
+++ src/org/cocolab/inpro/features/PitchAndPowerDumper.java
@@ -0,0 +1,19 @@
+package org.cocolab.inpro.features;
+
+import java.util.Locale;
+
+import org.cocolab.inpro.pitch.util.PitchUtils;
+import org.cocolab.inpro.sphinx.instrumentation.SignalFeatureListener;
+
+public class PitchAndPowerDumper implements SignalFeatureListener {
+
+	public void newSignalFeatures(double logEnergy, boolean voicing, double pitch) {
+		if (voicing) {
+			System.out.printf(Locale.US, "%.2f", pitch);
+		}
+		System.out.printf(Locale.US, "\t%.2f\n", logEnergy);
+	}
+
+	public void reset() { }
+
+}
