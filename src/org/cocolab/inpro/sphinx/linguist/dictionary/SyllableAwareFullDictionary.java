85884c4107b01ea1d1f8e9f173be2b2ad797b31b
Timo Baumann <baumann@informatik.uni-hamburg.de>
2009-07-02 10:32:32 +0000

    extend Sphinx' Dictionary and Pronunciation to handle syllable breaks (marked by "-") when reading a dictionary. This needs some slight modifications of sphinx.jar, a new one will be uploaded to emma shortly.
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1090 19460358-522e-0410-942e-fdb976ae3ca2

---
+++ src/org/cocolab/inpro/sphinx/linguist/dictionary/SyllableAwareFullDictionary.java
@@ -0,0 +1,90 @@
+/*
+ * Portions Copyright 2009 Timo Baumann, InPro Project
+ * 
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+package org.cocolab.inpro.sphinx.linguist.dictionary;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import edu.cmu.sphinx.linguist.acoustic.Unit;
+import edu.cmu.sphinx.linguist.acoustic.UnitManager;
+import edu.cmu.sphinx.linguist.dictionary.FullDictionary;
+import edu.cmu.sphinx.linguist.dictionary.Pronunciation;
+import edu.cmu.sphinx.util.ExtendedStreamTokenizer;
+
+public class SyllableAwareFullDictionary extends FullDictionary {
+
+    /** Spelling of the sentence start word. */
+    public static final String SYLLABLE_BOUNDARY_SYMBOL = "-";
+
+    /**
+     * Loads the given sphinx3 style simple dictionary from the given InputStream. The InputStream is assumed to contain
+     * ASCII data.
+     *
+     * @param inputStream  the InputStream of the dictionary
+     * @param isFillerDict true if this is a filler dictionary, false otherwise
+     * @throws java.io.IOException if there is an error reading the dictionary
+     */
+    @SuppressWarnings({"unchecked"})
+    protected Map<String, Object> loadDictionary(InputStream inputStream, boolean isFillerDict)
+            throws IOException {
+        Map<String, Object> dictionary = new HashMap<String, Object>();
+        ExtendedStreamTokenizer est = new ExtendedStreamTokenizer(inputStream,
+                true);
+        String word;
+        while ((word = est.getString()) != null) {
+            word = removeParensFromWord(word);
+            word = word.toLowerCase();
+            List<Unit> units = new ArrayList<Unit>(20);
+            String unitText;
+            List<Integer> syllBoundaries = new ArrayList<Integer>();
+            int counter = 0;
+            while ((unitText = est.getString()) != null) {
+            	if (unitText.equals(SYLLABLE_BOUNDARY_SYMBOL)) {
+            		syllBoundaries.add(Integer.valueOf(counter));
+            	} else {
+                    units.add(getCIUnit(unitText, isFillerDict));
+                    counter++;            		
+            	}
+            }
+            Unit[] unitsArray = units.toArray(new Unit[units.size()]);
+            List<Pronunciation> pronunciations = (List<Pronunciation>) dictionary.get(word);
+            if (pronunciations == null) {
+                pronunciations = new LinkedList<Pronunciation>();
+            }
+            Pronunciation pronunciation = new SyllableAwarePronunciation(unitsArray, syllBoundaries, null,
+                    null, 1.0f);
+            pronunciations.add(pronunciation);
+            // if we are adding a SIL ending duplicate
+            if (!isFillerDict && addSilEndingPronunciation) {
+                units.add(UnitManager.SILENCE);
+                Unit[] unitsArray2 = units.toArray(new Unit[units
+                        .size()]);
+                Pronunciation pronunciation2 = new SyllableAwarePronunciation(unitsArray2, syllBoundaries,
+                        null, null, 1.0f);
+                pronunciations.add(pronunciation2);
+            }
+            dictionary.put(word, pronunciations);
+        }
+        inputStream.close();
+        est.close();
+        createWords(dictionary, isFillerDict);
+        return dictionary;
+    }
+
+}
