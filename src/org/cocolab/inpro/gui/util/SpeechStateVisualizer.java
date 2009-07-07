b0fe41167a5aa77975272cdcfe19e473ef8888c5
Timo Baumann <baumann@informatik.uni-hamburg.de>
2009-07-07 12:11:24 +0000

    - move happyhal images to the right package - only display frame once recognition starts - add shutdown functionality to frame
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1102 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/gui/util/SpeechStateVisualizer.java
+++ src/org/cocolab/inpro/gui/util/SpeechStateVisualizer.java
@@ -5,8 +5,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import org.cocolab.inpro.gui.pentomino.p2.UserInterface;
-
 import edu.cmu.sphinx.frontend.BaseDataProcessor;
 import edu.cmu.sphinx.frontend.Data;
 import edu.cmu.sphinx.frontend.DataProcessingException;
@@ -15,21 +13,25 @@ import edu.cmu.sphinx.frontend.endpoint.SpeechClassifiedData;
 public class SpeechStateVisualizer extends BaseDataProcessor {
 
 	JLabel speechIndicator;
-	
+	JFrame f;
+
 	public SpeechStateVisualizer() {
-		ImageIcon silentIcon = new ImageIcon(UserInterface.class.getResource("happyhal-inactive.png"));
-		ImageIcon talkingIcon = new ImageIcon(UserInterface.class.getResource("happyhal-active.png"));
+		ImageIcon silentIcon = new ImageIcon(SpeechStateVisualizer.class.getResource("happyhal-inactive.png"));
+		ImageIcon talkingIcon = new ImageIcon(SpeechStateVisualizer.class.getResource("happyhal-active.png"));
 		speechIndicator = new JLabel(talkingIcon);
 		speechIndicator.setDisabledIcon(silentIcon);
 		speechIndicator.setEnabled(false);
-		JFrame f = new JFrame("your speech estimation");
+		f = new JFrame("your speech estimation");
 		f.add(speechIndicator);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
-		f.setVisible(true);		
 	}
-	
+
 	@Override
 	public Data getData() throws DataProcessingException {
+		if (!f.isVisible()) {
+			f.setVisible(true);
+		}
 		Data d = getPredecessor().getData();
 		if (d instanceof SpeechClassifiedData) {
 			SpeechClassifiedData scd = (SpeechClassifiedData) d;
