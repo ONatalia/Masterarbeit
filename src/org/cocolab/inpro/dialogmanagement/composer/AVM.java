e9ad64076d57edae765050885a06cc3b86b0b36b
Okko Buss <okkobuss@gmail.com>
2009-10-14 17:03:40 +0000

    More getters, setters, validators.
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1261 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/dialogmanagement/composer/AVM.java
+++ src/org/cocolab/inpro/dialogmanagement/composer/AVM.java
@@ -27,6 +27,12 @@ public class AVM {
 	}
 	
 	public boolean validateAttribute(String[] strs, String str) {
+		/*
+		 * Used by constructors and setters of AVM implementations.
+		 * Takes an array of valid Strings and a string as arguments.
+		 * Former is defined by AVM class.
+		 * If former contains latter, attribute is valid.
+		 */
 		for (String s: strs) {
 			if (s.equals(str)) {
 				return true;
@@ -35,6 +41,21 @@ public class AVM {
 		return false;
 	}
 
+	public boolean validateAttribute(AVM[] avms, AVM avm) {
+		/*
+		 * Used by constructors and setters of AVM implementations.
+		 * Takes an array of valid AVMs and an AVM as arguments.
+		 * Former is defined by AVM class.
+		 * If former contains latter, attribute is valid.
+		 */
+		for (AVM a: avms) {
+			if (avm.equals(a)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public String toString() {
 		return "";
 	}
