f87456ae260ecddc41879723e9272ae39f1c7b49
Timo Baumann <baumann@informatik.uni-hamburg.de>
2009-10-28 21:02:20 +0000

    ever wanted to find out about the performance of your ASR? be informed about which errors occur how often? store audio and recognition results for future reference? in case of an error, fix it immediately, in order to improve future versions of the acoustic model? no? well, then don't bother to look at this.
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1288 19460358-522e-0410-942e-fdb976ae3ca2

---
+++ src/test/org/cocolab/inpro/sphinx/train/DataCollector.java
@@ -0,0 +1,216 @@
+package test.org.cocolab.inpro.sphinx.train;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+
+import org.cocolab.inpro.incremental.listener.CurrentHypothesisViewer;
+import org.cocolab.inpro.sphinx.frontend.WavTEDLogger;
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
+public class DataCollector extends JFrame {
+	
+	private static final long serialVersionUID = 2871403525664635161L;
+
+	JToggleButton asrButton;
+	ResultPanel resultPanel;
+	
+	Recognizer recognizer;
+
+	private boolean recognizing;
+	
+	WavTEDLogger wavWriter;
+	
+	DataCollector() {
+		ConfigurationManager cm = new ConfigurationManager(DataCollector.class.getResource("config.xml"));
+
+		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+
+		JPanel asrControl = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+		CurrentHypothesisViewer chv = (CurrentHypothesisViewer) cm.lookup("hypViewer");
+		asrControl.add(chv.getTextField());
+		
+		final Microphone mic = (Microphone) cm.lookup("microphone");
+
+		final Icon playIcon = new ImageIcon(DataCollector.class.getResource("media-playback-start.png"), "start");
+		final Icon pauseIcon = new ImageIcon(DataCollector.class.getResource("media-playback-pause.png"), "stop");
+		asrButton = new JToggleButton(playIcon);
+		asrButton.setSelectedIcon(pauseIcon);
+		asrButton.setSelected(true);
+		asrButton.setEnabled(false);
+		asrButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (asrButton.isSelected()) {
+					System.out.println("Stopping microphone");
+					mic.stopRecording();
+				} else {
+					System.out.println("Starting microphone");
+					mic.startRecording();
+				}
+			}
+		});
+		
+		asrControl.add(asrButton);
+		this.add(asrControl);
+		
+		resultPanel = new ResultPanel();
+		resultPanel.setEnabled(false);
+		this.add(resultPanel);
+		
+		this.pack();
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		
+		recognizer = (Recognizer) cm.lookup("recognizer");
+		recognizer.allocate();
+		
+		wavWriter = (WavTEDLogger) cm.lookup("utteranceWavWriter");
+
+		mic.initialize();
+		setRecognizing(true);
+		Result result;
+		do {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (recognizing && mic.isRecording()) {
+				System.out.println("starting reco");
+				result = recognizer.recognize();
+				System.out.println("Reco is done" + result);
+				if ((result != null) && (result.getDataFrames() != null) && (result.getDataFrames().size() > 4)) {
+					newResult(result);
+				}
+			}
+		} while (true);
+	}
+	
+	void setRecognizing(boolean recognizing) {
+		if (recognizing) {
+			this.recognizing = true;
+			
+			asrButton.setEnabled(true);
+		} else {
+			System.exit(1);
+		}
+	}
+	
+	public void newResult(Result result) {
+		if (result.isFinal()) {
+			asrButton.setEnabled(false);
+			System.out.println(result.getBestResultNoFiller());
+			resultPanel.setEnabled(true);
+			resultPanel.newResult(result);
+		}
+	}
+
+	private static String getDataPrefix(String basePath) {
+/*		JFrame f = new JFrame();
+		f.setLayout(new GridLayout(3, 2));
+		f.add(new JLabel("Verzeichnis: "));
+		JTextField path = new JTextField(basePath);
+		f.add(path);
+		f.add(new JLabel("Dateipräfix: "));
+		JTextField name = new JTextField();
+		f.add(name);
+		JButton abort = new JButton("Abbrechen");
+		f.add(abort);
+		JButton ok = new JButton("Weiter");
+		f.add(ok);
+		f.setVisible(true);
+		f.pack();
+		return null;
+*/
+		return "/tmp/timo";
+	}
+	
+	static void saveTranscript(String transcript, String filename) {
+		try {
+			PrintStream outFile = new PrintStream(filename);
+			outFile.println(transcript);
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not write to " + filename);
+		}
+	}
+	
+	private class ResultPanel extends JPanel {
+		
+		private static final long serialVersionUID = 1L;
+		JTextField bestResult;
+		JButton submitButton;
+		JButton skipButton;
+		
+		ResultPanel() {
+			super(new FlowLayout(FlowLayout.LEFT));
+			
+			bestResult = new JTextField("", 35);
+			bestResult.setFont(new Font("Dialog", Font.BOLD, 24));
+			bestResult.setEditable(true);
+			this.add(bestResult);
+			
+			submitButton = new JButton(new ImageIcon(DataCollector.class.getResource("dialog-ok.png")));
+			submitButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					String orthoFilename = wavWriter.getMostRecentFilename().replaceFirst("\\.wav$", ".txt");
+					saveTranscript(bestResult.getText(), orthoFilename);
+					goBackToRecording();
+				}
+			});
+			this.add(submitButton);
+			
+			skipButton = new JButton(new ImageIcon(DataCollector.class.getResource("dialog-cancel.png")));
+			this.add(skipButton);
+			skipButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					System.err.println("Skipping text" + bestResult.getText());
+					goBackToRecording();
+				}
+			});
+		}
+		
+		void goBackToRecording() {
+			this.setEnabled(false);
+			asrButton.setEnabled(true);
+			setRecognizing(true);
+		}
+		
+		public void setEnabled(boolean enabled) {
+			super.setEnabled(enabled);
+			bestResult.setEnabled(enabled);
+			submitButton.setEnabled(enabled);
+			skipButton.setEnabled(enabled);
+		}
+		
+		void newResult(Result result) {
+			String resultText = result.getBestFinalToken().getWordPath();
+			bestResult.setText(resultText);
+			String hypFilename = wavWriter.getMostRecentFilename().replaceFirst("\\.wav$", ".hyp");
+			saveTranscript(resultText, hypFilename);
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		new DataCollector();	
+	}
+	
+}
