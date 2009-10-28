0e2c8e541a8ec075927c270ccd91e20cf5ef99c3
Timo Baumann <baumann@informatik.uni-hamburg.de>
2009-10-28 12:22:32 +0000

    oups, forgot one file. also: HypothesisChangeListener can now be configure to not show its own window
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1287 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/incremental/listener/CurrentHypothesisViewer.java
+++ src/org/cocolab/inpro/incremental/listener/CurrentHypothesisViewer.java
@@ -11,20 +11,36 @@ import org.cocolab.inpro.incremental.unit.EditMessage;
 import org.cocolab.inpro.incremental.unit.IU;
 import org.cocolab.inpro.incremental.unit.WordIU;
 
+import edu.cmu.sphinx.util.props.PropertyException;
+import edu.cmu.sphinx.util.props.PropertySheet;
+import edu.cmu.sphinx.util.props.S4Boolean;
+
 public class CurrentHypothesisViewer extends HypothesisChangeListener {
 
-	JFrame f;
+	@S4Boolean(defaultValue = true)
+    public final static String PROP_SHOW_WINDOW = "showWindow";
+	
 	JTextField textField;
 	
 	public CurrentHypothesisViewer() {
 		textField = new JTextField("", 35);
 		textField.setEditable(false);
 		textField.setFont(new Font("Dialog", Font.BOLD, 24));
-		f = new JFrame("current hypothesis");
-		f.add(textField);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.pack();
-		f.setVisible(true);
+	}
+	
+	@Override
+	public void newProperties(PropertySheet ps) throws PropertyException {
+		if (ps.getBoolean(PROP_SHOW_WINDOW)) {
+			JFrame f = new JFrame("current hypothesis");
+			f.add(textField);
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			f.pack();
+			f.setVisible(true);
+		}
+	}
+	
+	public JTextField getTextField() {
+		return textField;
 	}
 	
 	@Override
--- src/org/cocolab/inpro/incremental/listener/HypothesisChangeListener.java
+++ src/org/cocolab/inpro/incremental/listener/HypothesisChangeListener.java
@@ -17,12 +17,11 @@
  */
 package org.cocolab.inpro.incremental.listener;
 
-import java.util.Collection;
 import java.util.List;
 
-import org.cocolab.inpro.incremental.ASRResultKeeper;
+import org.cocolab.inpro.incremental.PushBuffer;
+import org.cocolab.inpro.incremental.deltifier.ASRResultKeeper;
 import org.cocolab.inpro.incremental.unit.EditMessage;
-import org.cocolab.inpro.incremental.unit.IU;
 import org.cocolab.inpro.incremental.unit.WordIU;
 
 import edu.cmu.sphinx.instrumentation.Resetable;
@@ -30,15 +29,9 @@ import edu.cmu.sphinx.util.props.Configurable;
 import edu.cmu.sphinx.util.props.PropertyException;
 import edu.cmu.sphinx.util.props.PropertySheet;
 
-public abstract class HypothesisChangeListener implements Configurable, Resetable {
+public abstract class HypothesisChangeListener implements Configurable, Resetable, PushBuffer {
 
 	int currentFrame = 0;
-
-	/*
-	 * this should receive a list of current IUs and 
-	 * a list of edit messages since the last call to hypChange
-	 */
-	public abstract void hypChange(Collection<? extends IU> ius, List<? extends EditMessage<? extends IU>> edits);
 
 	public void hypChange(ASRResultKeeper deltifier) {
 		List<EditMessage<WordIU>> edits = deltifier.getWordEdits();
