1c60c17806998c0f0361ea7ab26c37eb9c268e77
Timo Baumann <baumann@informatik.uni-hamburg.de>
2010-02-09 15:53:57 +0000

    example of a package-info file.
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1454 19460358-522e-0410-942e-fdb976ae3ca2

---
+++ src/org/cocolab/inpro/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Root for all the code developed in the inpro project at the University of Potsdam.
+ *
+ * The inpro project, blabla balabala
+ * 
+ * @author Timo Baumann
+ * @author Okko Buß 
+ * @author David Schlangen
+ *
+<pre>
+_____________________________________________________________
+  _                                         _           _   
+ (_)_ __  _ __  _ __ ___    _ __  _ __ ___ (_) ___  ___| |_ 
+ | | '_ \| '_ \| '__/ _ \  | '_ \| '__/ _ \| |/ _ \/ __| __|
+ | | | | | |_) | | | (_) | | |_) | | | (_) | |  __/ (__| |_ 
+ |_|_| |_| .__/|_|  \___/  | .__/|_|  \___// |\___|\___|\__|
+         |_|               |_|           |__/               
+_____________________________________________________________
+</pre>
+ */
+package org.cocolab.inpro;
+
