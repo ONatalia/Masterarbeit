b88fd6c889b05e7c6739d3dd89d1862f03d2cdb3
Timo Baumann <baumann@informatik.uni-hamburg.de>
2010-03-24 11:07:54 +0000

    - setting now optionally sends manipulations via OAA (to display changes on another computer) - exchange AgentImpl with Agent(interface) in OAA*Action - some refactoring and a lot of new functionality in WozGUI - new OAAGameCanvas, which is controlled via OAA
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1537 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/gui/util/OAAAction.java
+++ src/org/cocolab/inpro/gui/util/OAAAction.java
@@ -14,6 +14,7 @@ import org.apache.log4j.PropertyConfigurator;
 import antlr_oaa.RecognitionException;
 import antlr_oaa.TokenStreamException;
 
+import com.sri.oaa2.agentlib.Agent;
 import com.sri.oaa2.agentlib.AgentException;
 import com.sri.oaa2.agentlib.AgentImpl;
 import com.sri.oaa2.icl.IclList;
@@ -29,10 +30,10 @@ public class OAAAction extends AbstractAction {
 	// globally set params for sending solve-requests  
 	public static IclTerm params = new IclList(new IclStruct("reply", new IclStr("none")));
 
-	AgentImpl agent;
+	Agent agent;
 	protected List<IclTerm> calls;
 	
-	public OAAAction(AgentImpl agent, String name, String goal) throws RecognitionException, TokenStreamException {
+	public OAAAction(Agent agent, String name, String goal) throws RecognitionException, TokenStreamException {
 		super(name, null);
 		logger.setLevel(Level.ALL);
 		this.agent = agent;
@@ -40,14 +41,14 @@ public class OAAAction extends AbstractAction {
 		calls.add(IclTerm.fromString(goal));
 	}
 	
-	public OAAAction(AgentImpl agent, String name, Icon icon, String goal) throws RecognitionException, TokenStreamException {
+	public OAAAction(Agent agent, String name, Icon icon, String goal) throws RecognitionException, TokenStreamException {
 		super(name, icon);
 		this.agent = agent;
 		calls = new LinkedList<IclTerm>();
 		calls.add(IclTerm.fromString(goal));
 	}
 	
-	public OAAAction(AgentImpl agent, String name, Icon icon, String goal1, String goal2) throws RecognitionException, TokenStreamException {
+	public OAAAction(Agent agent, String name, Icon icon, String goal1, String goal2) throws RecognitionException, TokenStreamException {
 		this(agent, name, icon, goal1);
 		calls.add(IclTerm.fromString(goal2));
 	}
--- src/org/cocolab/inpro/gui/util/OAADispatchAction.java
+++ src/org/cocolab/inpro/gui/util/OAADispatchAction.java
@@ -3,14 +3,14 @@ package org.cocolab.inpro.gui.util;
 import antlr_oaa.RecognitionException;
 import antlr_oaa.TokenStreamException;
 
-import com.sri.oaa2.agentlib.AgentImpl;
+import com.sri.oaa2.agentlib.Agent;
 
 @SuppressWarnings("serial")
 public class OAADispatchAction extends OAAAction {
 
 	public static String DISPATCH_GOAL_PREFIX = "x";
 	
-	public OAADispatchAction(AgentImpl agent, String name, String filename) throws RecognitionException, TokenStreamException {
-		super(agent, name, null, DISPATCH_GOAL_PREFIX + "PlayFile('" + filename + "')");
+	public OAADispatchAction(Agent oaaAgent, String name, String filename) throws RecognitionException, TokenStreamException {
+		super(oaaAgent, name, null, DISPATCH_GOAL_PREFIX + "PlayFile('" + filename + "')");
 	}
 }
