6171b6a4b5aaec7be5a383e6df23015042468a0a
Timo Baumann <baumann@informatik.uni-hamburg.de>
2010-04-27 09:35:24 +0000

    factored out IUDocument to its own toplevel class (making SimpleText much more readable)
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@1706 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/apps/SimpleText.java
+++ src/org/cocolab/inpro/apps/SimpleText.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -14,9 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.PlainDocument;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
@@ -25,10 +22,7 @@ import org.cocolab.inpro.incremental.PushBuffer;
 import org.cocolab.inpro.incremental.listener.HypothesisChangeListener;
 import org.cocolab.inpro.incremental.processor.CurrentASRHypothesis;
 import org.cocolab.inpro.incremental.processor.FloorManager;
-import org.cocolab.inpro.incremental.unit.TextualWordIU;
-import org.cocolab.inpro.incremental.unit.EditMessage;
-import org.cocolab.inpro.incremental.unit.EditType;
-import org.cocolab.inpro.incremental.unit.IUList;
+import org.cocolab.inpro.incremental.util.IUDocument;
 
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertySheet;
@@ -84,7 +78,6 @@ public class SimpleText extends JPanel implements ActionListener {
 	
 	SimpleText(FloorManager floorManager) {
 		iuDocument = new IUDocument();
-		iuDocument.listeners = new ArrayList<PushBuffer>();
 		textField = new JTextField(40);
 		textField.setFont(new Font("Dialog", Font.BOLD, 24));
 		textField.setDocument(iuDocument);
@@ -94,7 +87,6 @@ public class SimpleText extends JPanel implements ActionListener {
 		add(textField);
 		add(commitButton);
 		add(floorManager.signalPanel);
-		
 	}
 	
 	public void actionPerformed(ActionEvent ae) {
@@ -163,147 +155,4 @@ public class SimpleText extends JPanel implements ActionListener {
     	}
 	}
 
-	/**
-	 * An IUDocument stores a list of current IUs, 
-	 * edits since the last update and
-	 * the string for the (partial) next IU
-	 * 
-	 * It handles HypothesisChangeListeners 
-	 * which are notified, when the IUList changes (or is committed)
-	 * (and can be set via setListeners())
-	 * 
-	 * IUs are committed and the list is reset after an explicit call to commit()
-	 * (when used as the document of a JTextField, this can be done by 
-	 *  calling commit() from a JTextField's ActionListener)
-	 *  
-	 * @see javax.swing.text.Document Document
-	 * @author timo
-	 */
-	static class IUDocument extends PlainDocument {
-
-		List<PushBuffer> listeners;
-		IUList<TextualWordIU> wordIUs = new IUList<TextualWordIU>();
-		List<EditMessage<TextualWordIU>> edits = new ArrayList<EditMessage<TextualWordIU>>();
-		String currentWord = "";
-		
-		int currentFrame = 0;
-		
-		public void setListeners(List<PushBuffer> listeners) {
-			this.listeners = listeners;
-		}
-		
-		public void notifyListeners() {
-			if (edits.size() > 0) {
-				//logger.debug("notifying about" + edits);
-				currentFrame += 100;
-				for (PushBuffer listener : listeners) {
-					if (listener instanceof HypothesisChangeListener) {
-						((HypothesisChangeListener) listener).setCurrentFrame(currentFrame);
-					}
-					// notify
-					if (wordIUs != null && edits != null)
-						listener.hypChange(wordIUs, edits);
-					
-				}
-				edits = new ArrayList<EditMessage<TextualWordIU>>();
-			}
-		}
-		
-		private void addCurrentWord() {
-			//logger.debug("adding " + currentWord);
-			TextualWordIU sll = (wordIUs.size() > 0) ? wordIUs.get(wordIUs.size() - 1) : TextualWordIU.FIRST_ATOMIC_WORD_IU;
-			TextualWordIU iu = new TextualWordIU(currentWord, sll);
-			EditMessage<TextualWordIU> edit = new EditMessage<TextualWordIU>(EditType.ADD, iu);
-			edits.add(edit);
-			wordIUs.add(iu);
-			//logger.debug(edit.toString());
-			currentWord = "";
-		}
-		
-		public void commit() {
-			// handle last word (if there is one)
-			if (!"".equals(currentWord)) {
-				addCurrentWord();
-			}
-			// add commit messages
-			for (TextualWordIU iu : wordIUs) {
-				edits.add(new EditMessage<TextualWordIU>(EditType.COMMIT, iu));
-				iu.update(EditType.COMMIT);
-			}
-			// notify
-			notifyListeners();
-			// reset
-			try {
-				super.remove(0,getLength());
-			} catch (BadLocationException e) {
-				e.printStackTrace();
-			}
-			wordIUs = new IUList<TextualWordIU>();
-			edits = new ArrayList<EditMessage<TextualWordIU>>();			
-		}
-		
-		/* Overrides over PlainDocument: */
-		
-		/** 
-		 * only allow removal at the right end
-		 * and correctly handle removals beyond the current word
-		 */
-		@Override
-		public void remove(int offs, int len) throws BadLocationException {
-			if (offs + len == getLength()) { // only allow removal at the right end
-				super.remove(offs, len);
-//				if (getText(getLength() - 1, 1).)
-				while (len > currentWord.length()) { // +1 because the whitespace has to be accounted for
-					len -= currentWord.length();
-					len--; // to account for whitespace
-					TextualWordIU iu = wordIUs.remove(wordIUs.size() - 1);
-					EditMessage<TextualWordIU> edit = new EditMessage<TextualWordIU>(EditType.REVOKE, iu);
-					edits.add(edit);
-					//logger.debug(edit.toString());
-					currentWord = iu.getWord();
-				}
-				currentWord = currentWord.substring(0, currentWord.length() - len);
-				//logger.debug("now it's " + currentWord);
-				notifyListeners();
-			}
-		}
-
-		/** 
-		 * only allow insertion at the right end
-		 */
-		@Override
-		public void insertString(int offs, String str, AttributeSet a)
-				throws BadLocationException {
-			if (offs == getLength()) {
-				/* character by character: 
-				 *    check validity (for example, we don't like multiple whitespace)
-				 *    add them to the current word, if it's not whitespace
-				 *    add current word to IUList if it is whitespace
-				 * finally, add the (possibly changed characters) to the superclass's data handling
-				 */
-				char[] chars = str.toCharArray();
-				String outStr = "";
-				for (char ch : chars) {
-					if (Character.isWhitespace(ch)) {
-						if (currentWord.length() > 0) {
-							addCurrentWord();
-							outStr += " "; // add a space, no matter what whitespace was added
-						} else {
-							//logger.debug("ignoring additional whitespace");
-						}
-					} else {
-						//logger.debug("appending to currentWord");
-						currentWord += ch;
-						//logger.debug("now it's " + currentWord);
-						outStr += ch;
-					}
-					
-				}
-				super.insertString(offs, outStr, a);
-			}
-			notifyListeners();
-		}
-		
-	}
-	
 }
