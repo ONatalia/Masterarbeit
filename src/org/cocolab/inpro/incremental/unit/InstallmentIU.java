23bf3591d8c1db6bc9e43a188c1b7a7fcc07af39
Okko Buss <okkobuss@gmail.com>
2010-11-08 13:42:14 +0000

    InstallmentIU: plus javadocity DM: minus one nasty little one-line bug. Pento config updated and some ASR resources added.
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@2173 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/incremental/unit/InstallmentIU.java
+++ src/org/cocolab/inpro/incremental/unit/InstallmentIU.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * a simple wrapper for both system and user uttered installments
+ * A simple wrapper for both system and user uttered installments
  * - for user installments references to recognized WordIUs are kept 
  *   used to infer the spoken text in toPayload()
  * - for system installments a reference to the corresponding dialogueAct
@@ -14,30 +14,67 @@ import java.util.List;
  */
 public class InstallmentIU extends IU {
 
+	/**
+	 * String holding the system reponse
+	 */
 	final String tts;
-	
+	/**
+	 * Variable specifying who produced this installment
+	 */
 	boolean systemProduced; // true: system utterance, false: user utterance
+
+	/**
+	 * The first InstalmentIUs
+	 */
+	public static final InstallmentIU FIRST_USER_INSTALLMENT_IU = new InstallmentIU(DialogueActIU.FIRST_DA_IU, null);
+	public static final InstallmentIU FIRST_SYSTEM_INSTALLMENT_IU = new InstallmentIU(Collections.<WordIU>emptyList());
 	
+	/**
+	 * Constructor for an installment produced by the user, grounded
+	 * in a list of spoken words.
+	 * @param currentInstallment the list of wordIUs grounding this installment
+	 */
 	public InstallmentIU(List<WordIU> currentInstallment) {
 		super(currentInstallment);
 		this.systemProduced = false;
 		this.tts = null;
 	}
-	
+
+	/**
+	 * Constructor for an installment produced by the system, grounded
+	 * in a dialogueAct.
+	 * @param dialogueAct the dialogue act grounding this installment
+	 * @param tts the spoken string
+	 */
 	public InstallmentIU(DialogueActIU dialogueAct, String tts) {
 		super(Collections.<IU>singletonList(dialogueAct));
 		this.systemProduced = true;
 		this.tts = tts;
 	}
-	
+
+	/**
+	 * Determines whether the installment is a 'system' one.
+	 * @return true if produced by the system, false if by the user.
+	 */
 	public boolean systemProduced() {
 		return systemProduced;
 	}
 	
+	/**
+	 * Determines whether the installment is a 'user' one.
+	 * @return true if produced by the user, false if by the system.
+	 */
 	public boolean userProduced() {
 		return !systemProduced;
 	}
 
+	/**
+	 * Returns a string representation of the payload of this IU.
+	 * For user-produced installments, the payload is a String representation
+	 * of the spoken words grounding this IU.
+	 * For system-produced installments, it's a string representation of
+	 * the spoken output grounded in this IU.
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public String toPayLoad() {
