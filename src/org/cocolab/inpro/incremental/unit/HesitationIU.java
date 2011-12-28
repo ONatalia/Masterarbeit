3f94cf382cce5212987cbe86a2b9259727f1f124
Timo Baumann <baumann@informatik.uni-hamburg.de>
2011-12-28 19:19:02 +0000

    rewritten HesitationIU
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@2495 19460358-522e-0410-942e-fdb976ae3ca2

--- src/org/cocolab/inpro/incremental/unit/HesitationIU.java
+++ src/org/cocolab/inpro/incremental/unit/HesitationIU.java
@@ -1,62 +1,38 @@
 package org.cocolab.inpro.incremental.unit;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.cocolab.inpro.annotation.Label;
+import org.cocolab.inpro.apps.SimpleMonitor;
 import org.cocolab.inpro.audio.DDS16kAudioInputStream;
 import org.cocolab.inpro.audio.DispatchStream;
 import org.cocolab.inpro.tts.MaryAdapter;
-import org.cocolab.inpro.tts.MaryAdapter4internal;
-import org.cocolab.inpro.tts.PitchMark;
-import org.cocolab.inpro.tts.hts.FullPFeatureFrame;
-import org.cocolab.inpro.tts.hts.FullPStream;
 import org.cocolab.inpro.tts.hts.IUBasedFullPStream;
 import org.cocolab.inpro.tts.hts.VocodingAudioStream;
 
-import test.org.cocolab.inpro.synthesis.PatternDemonstrator;
-
-public class HesitationIU extends SysSegmentIU {
+public class HesitationIU extends WordIU {
 	
-	private static final int DEFAULT_DURATION = 300; // ms
+	public static SysInstallmentIU protoHesitation = new SysInstallmentIU("ähm");
 
-	public HesitationIU(SegmentIU sll) {
-		super(new Label(0.0, DEFAULT_DURATION * 0.001 + 0.1, "<hes>"), null);
-		setSameLevelLink(sll);
-		System.err.println(toMaryXML());
-		FullPStream pstream = ((MaryAdapter4internal) MaryAdapter.getInstance()).maryxml2hmmFeatures(toMaryXML());
-		List<FullPFeatureFrame> hmmSynthesisFeatures = pstream.getFullFrames(20, 80);
-		for (FullPFeatureFrame frame : hmmSynthesisFeatures) {
-			frame.setlf0Par(5f);
+	@SuppressWarnings({ "unchecked", "rawtypes" }) // the cast for GRINs
+	public HesitationIU(WordIU sll) {
+		super("<hes>", sll, (List) protoHesitation.getSegments());
+		for (IU seg : groundedIn) {
+			((SysSegmentIU) seg).stretch(2);
 		}
-		setHmmSynthesisFrames(hmmSynthesisFeatures);
-		//stretch(1.5);
-	}
-	
-	@Override
-	public void appendMaryXML(StringBuilder sb) {
-		List<PitchMark> pms = ((SysSegmentIU) getSameLevelLink()).pitchMarks;
-		sb.append("<ph d='");
-		sb.append(DEFAULT_DURATION);
-		sb.append("' f0='");
-		sb.append(pms.get(pms.size() - 1).toString());
-		sb.append("' p='E' end='0.4'/>");
-	}
-	
-	public String toMaryXML() {
-		StringBuilder sb = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"de\">\n<p>\n<s>\n<phrase>\n<t>\n<syllable>\n");
-		((SegmentIU) getSameLevelLink()).appendMaryXML(sb);
-		appendMaryXML(sb);
-		sb.append("</syllable>\n</t>\n<boundary duration='500' />\n</phrase>\n</s>\n</p>\n</maryxml>");
-		return sb.toString();
+		protoHesitation.addFeatureStreamToSegmentIUs();
+		protoHesitation.scaleDeepCopyAndStartAtZero(1f); // create new IU substructure for the next protohesitation
 	}
 
 	public static void main(String[] args) {
 		MaryAdapter.getInstance();
-		DispatchStream dispatcher = PatternDemonstrator.setupDispatcher();
-		HesitationIU hes = new HesitationIU(new SysSegmentIU(new Label(0.0, 0.1, "n"), Collections.<PitchMark>singletonList(new PitchMark("(0,100)"))));
-		dispatcher.playStream(new DDS16kAudioInputStream(new VocodingAudioStream(new IUBasedFullPStream(hes), true)), true);
+		DispatchStream dispatcher = SimpleMonitor.setupDispatcher();
+		HesitationIU hes = new HesitationIU(null);// HesitationIU(new SysSegmentIU(new Label(0.0, 0.1, "b"), Collections.<PitchMark>singletonList(new PitchMark("(0,100)"))));
+		dispatcher.playStream(new DDS16kAudioInputStream(new VocodingAudioStream(new IUBasedFullPStream(hes.getSegments().get(0)), true)), true);
+		hes = new HesitationIU(null);// HesitationIU(new SysSegmentIU(new Label(0.0, 0.1, "b"), Collections.<PitchMark>singletonList(new PitchMark("(0,100)"))));
+		dispatcher.playStream(new DDS16kAudioInputStream(new VocodingAudioStream(new IUBasedFullPStream(hes.getSegments().get(0)), true)), false);
+		hes = new HesitationIU(null);// HesitationIU(new SysSegmentIU(new Label(0.0, 0.1, "b"), Collections.<PitchMark>singletonList(new PitchMark("(0,100)"))));
+		dispatcher.playStream(new DDS16kAudioInputStream(new VocodingAudioStream(new IUBasedFullPStream(hes.getSegments().get(0)), true)), false);
 		dispatcher.shutdown();
 	}
-	
+
 }
