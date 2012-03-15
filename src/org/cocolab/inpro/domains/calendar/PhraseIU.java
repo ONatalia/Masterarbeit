752c087e36b1116419f062b4e52460f0c46a1e00
Timo Baumann <baumann@informatik.uni-hamburg.de>
2012-03-15 11:14:06 +0000

    initial code for the SynthesisModule and a PhraseIU dummy
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@2581 19460358-522e-0410-942e-fdb976ae3ca2

---
+++ src/org/cocolab/inpro/domains/calendar/PhraseIU.java
@@ -0,0 +1,12 @@
+package org.cocolab.inpro.domains.calendar;
+
+import org.cocolab.inpro.incremental.unit.IU;
+
+public class PhraseIU extends IU {
+
+	@Override
+	public String toPayLoad() {
+		return "some payload";
+	}
+
+}
---
+++ src/org/cocolab/inpro/domains/calendar/SynthesisModule.java
@@ -0,0 +1,59 @@
+package org.cocolab.inpro.domains.calendar;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.cocolab.inpro.apps.SimpleMonitor;
+import org.cocolab.inpro.audio.DispatchStream;
+import org.cocolab.inpro.incremental.IUModule;
+import org.cocolab.inpro.incremental.unit.EditMessage;
+import org.cocolab.inpro.incremental.unit.IU;
+
+import edu.cmu.sphinx.util.props.PropertyException;
+import edu.cmu.sphinx.util.props.PropertySheet;
+
+public class SynthesisModule extends IUModule {
+
+	DispatchStream speechDispatcher;
+//	DispatchStream noiseDispatcher;
+
+	@Override
+	public void newProperties(PropertySheet ps) throws PropertyException {
+		super.newProperties(ps);
+		speechDispatcher = SimpleMonitor.setupDispatcher();
+	}
+	
+	@Override
+	protected void leftBufferUpdate(Collection<? extends IU> ius,
+			List<? extends EditMessage<? extends IU>> edits) {
+		for (EditMessage<?> em : edits) {
+			switch (em.getType()) {
+			case ADD:
+				System.err.println("I should append " + em.getIU().toPayLoad() + " to what I'm saying.");
+				break;
+			case REVOKE:
+				System.err.println("I should NOT say " + em.getIU().toPayLoad());
+				break;
+			}
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SynthesisModule sm = new SynthesisModule();
+		List<PhraseIU> phrases = new ArrayList<PhraseIU>();
+		phrases.add(new PhraseIU());
+		sm.rightBuffer.setBuffer(phrases);
+		sm.rightBuffer.notify(sm);
+		phrases.add(new PhraseIU());
+		sm.rightBuffer.setBuffer(phrases);
+		sm.rightBuffer.notify(sm);
+		phrases.remove(1);
+		sm.rightBuffer.setBuffer(phrases);
+		sm.rightBuffer.notify(sm);		
+	}
+
+}
