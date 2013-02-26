610261f6b207a085938ba7f255727da7a2571c48
Casey Kennington <ckennington@techfak.uni-bielefeld.de>
2013-02-26 13:04:45 +0000

    updated LabelWriter can write to file, as specified in config file. If not specified, then it functions as it always has.
    git-svn-id: svn+ssh://gate.spectrum.uni-bielefeld.de/homes/tbaumann/inpro-svnrepo/trunk@3268 19460358-522e-0410-942e-fdb976ae3ca2

--- src/inpro/incremental/sink/LabelWriter.java
+++ src/inpro/incremental/sink/LabelWriter.java
@@ -1,12 +1,23 @@
 package inpro.incremental.sink;
 
 import inpro.incremental.unit.EditMessage;
+import inpro.incremental.unit.EditType;
 import inpro.incremental.unit.IU;
 import inpro.util.TimeUtil;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+
+import edu.cmu.sphinx.util.props.PropertyException;
+import edu.cmu.sphinx.util.props.PropertySheet;
+import edu.cmu.sphinx.util.props.S4Boolean;
+import edu.cmu.sphinx.util.props.S4String;
+
+
 
 
 /**
@@ -16,17 +27,88 @@ import java.util.Locale;
  * @author timo
  */
 public class LabelWriter extends FrameAwarePushBuffer {
+	
+	@S4Boolean(defaultValue = false)
+    public final static String PROP_WRITE_FILE = "writeToFile";
+	
+	@S4Boolean(defaultValue = true)
+    public final static String PROP_WRITE_STDOUT = "writeToStdOut";
+	
+    @S4String(defaultValue = "")
+    public final static String PROP_FILE_PATH= "filePath";    
+    
+    @S4String(defaultValue = "")
+    public final static String PROP_FILE_NAME = "fileName";    
+    
+    private boolean writeToFile;
+    private boolean writeToStdOut;
+    private String filePath;
+    private String fileName;
+    int frameOutput = -1;
+
+	
+	@Override	
+	public void newProperties(PropertySheet ps) throws PropertyException {
+		writeToFile = ps.getBoolean(PROP_WRITE_FILE);
+		writeToStdOut = ps.getBoolean(PROP_WRITE_STDOUT);
+		filePath = ps.getString(PROP_FILE_PATH);
+		fileName = ps.getString(PROP_FILE_NAME);
+	}
 
 	@Override
 	public void hypChange(Collection<? extends IU> ius, List<? extends EditMessage<? extends IU>> edits) {
-		if (ius.size() > 0) {
-			System.out.println(String.format(Locale.US, "Time: %.2f", 
-					currentFrame * TimeUtil.FRAME_TO_SECOND_FACTOR));
-			for (IU iu : ius) {
-				System.out.println(iu.toLabelLine());
+		
+		/*
+		 * Get the time first
+		 */
+		String toOut = String.format(Locale.US, "Time: %.2f", 
+				currentFrame * TimeUtil.FRAME_TO_SECOND_FACTOR);
+		
+		
+		/*
+		 * Then go through all the IUs, ignoring commits 
+		 */
+		boolean added = false;
+		for (IU iu : ius) {
+			if (iu.isCommitted()) continue;
+			added = true;
+			toOut += "\n" + iu.toLabelLine();
+		}
+		toOut += "\n\n";
+		
+		/*
+		 * If there were only commits, or if there are not IUs, then print out as specified
+		 */
+		if (ius.size() > 0 && added && frameOutput != currentFrame) {
+			frameOutput = currentFrame;
+
+			if (writeToFile) {
+				try {
+				FileWriter writer = new FileWriter(filePath + "/" + fileName + ".inc_reco", true);
+				writer.write(toOut);
+				writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-			System.out.println();
+			
+			if (writeToStdOut) {
+				System.out.println(toOut);
+			}
 		}
 	}
+	
+	/*
+	 * A file name can be specified here, if not specified in the config file
+	 */
+	public void setFileName(String name) {
+		fileName = name;
+	}
 
+	@Override
+	public void reset() {
+		super.reset();
+		frameOutput = -1;
+	}
+	
 }
