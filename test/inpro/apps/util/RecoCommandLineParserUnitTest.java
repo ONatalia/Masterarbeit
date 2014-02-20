9a6648742fed835a34696a02a4b122748329f078
Sören Nykamp <0nykamp@informatik.uni-hamburg.de>
2014-02-20 15:02:24 +0100

    Some more testing for RecoCommandLineParser

--- test/inpro/apps/util/RecoCommandLineParserUnitTest.java
+++ test/inpro/apps/util/RecoCommandLineParserUnitTest.java
@@ -48,5 +48,37 @@ public class RecoCommandLineParserUnitTest {
 		assertEquals(root + "/test/data/empty.gram", pars.languageModelURL.getPath());
 		assertTrue(pars.parsedSuccessfully());
 	}
+	
+	@Test
+	public void testOtherArguments()
+	{
+		String[] args = {"-M", "-O", "-v", "-f", "-N"};
+		RecoCommandLineParser pars = new RecoCommandLineParser(args);
+		assertEquals(CommonCommandLineParser.MICROPHONE_INPUT,pars.getInputMode());
+		assertEquals(CommonCommandLineParser.DISPATCHER_OBJECT_OUTPUT, pars.outputMode);
+		assertTrue(pars.verbose());
+		assertTrue(pars.ignoreErrors());
+		assertEquals(RecoCommandLineParser.NON_INCREMENTAL, pars.incrementalMode);
+		assertTrue(pars.parsedSuccessfully());
+		
+		String[] args2 = {"-R", "22", "-T", "-Is", "5", "-rt"};
+		RecoCommandLineParser pars2 = new RecoCommandLineParser(args2);
+		assertEquals(CommonCommandLineParser.RTP_INPUT, pars2.getInputMode());
+		assertEquals(CommonCommandLineParser.TED_OUTPUT, pars2.outputMode);
+		assertEquals(22, pars2.rtpPort);
+		assertFalse(pars2.verbose());
+		assertFalse(pars2.ignoreErrors());
+		assertEquals(RecoCommandLineParser.SMOOTHED_INCREMENTAL, pars2.incrementalMode);
+		assertEquals(5, pars2.incrementalModifier);
+		assertTrue(pars2.parsedSuccessfully());
+		
+		String[] args3 = {"-M", "-L", "-If", "5"};
+		RecoCommandLineParser pars3 = new RecoCommandLineParser(args3);
+		assertEquals(CommonCommandLineParser.LABEL_OUTPUT,pars3.outputMode);
+		assertEquals(RecoCommandLineParser.FIXEDLAG_INCREMENTAL, pars3.incrementalMode);
+		assertEquals(5, pars3.incrementalModifier);
+		assertTrue(pars.parsedSuccessfully());
+		
+	}
 
 }
