57c9a5a9d88f7b50582150d5fd8a4b1c0f378edb
Casey Kennington <ckennington@cit-ec.uni-bielefeld.de>
2014-10-31 15:39:59 +0100

    InstantOListenerModule now uses venice.lib, tested and works

--- src/inpro/io/instantio/InstantIOListenerModule.java
+++ src/inpro/io/instantio/InstantIOListenerModule.java
@@ -1,19 +1,13 @@
 package inpro.io.instantio;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import inpro.incremental.IUModule;
-import inpro.incremental.unit.EditMessage;
-import inpro.incremental.unit.EditType;
-import inpro.incremental.unit.IU;
 import inpro.io.ListenerModule;
-import inpro.io.SensorIU;
 
 import org.apache.log4j.Logger;
-import org.instantreality.InstantIO.InSlot;
 
+import venice.lib.AbstractSlot;
+import venice.lib.AbstractSlotListener;
+import venice.lib.networkIIO.IIONamespaceBuilder;
+import venice.lib.networkIIO.IIONamespaceBuilder.Task;
 import edu.cmu.sphinx.util.props.PropertyException;
 import edu.cmu.sphinx.util.props.PropertySheet;
 import edu.cmu.sphinx.util.props.S4String;
@@ -22,7 +16,7 @@ import edu.cmu.sphinx.util.props.S4String;
  * @author casey
  *
  */
-public class InstantIOListenerModule extends ListenerModule implements InSlot.Listener {
+public class InstantIOListenerModule extends ListenerModule implements AbstractSlotListener {
 	
 	static Logger log = Logger.getLogger(InstantIOListenerModule.class.getName());
 	
@@ -30,45 +24,37 @@ public class InstantIOListenerModule extends ListenerModule implements InSlot.Li
 	public final static String INSLOT_PROP = "inslot";
 	
  	@S4String(defaultValue = "")
-	public final static String ID_PROP = "id";	
+	public final static String ID_PROP = "id";
+ 	
+ 	private String inSlot;
 	
 	@Override
 	public void newProperties(PropertySheet ps) throws PropertyException {
 		super.newProperties(ps);
 		log.info("Setting up InstantIOListenerModule");
-		String inslot = ps.getString(INSLOT_PROP);
+		inSlot = ps.getString(INSLOT_PROP);
+		String id = ps.getString(ID_PROP);
+		System.out.println(id + " " + inSlot);
+		IIONamespaceBuilder.setTask(Task.READ);
+		
+		IIONamespaceBuilder.prepareNamespace("");
+		
+		AbstractSlot[] slots = new AbstractSlot[1];
+		slots[0] = new AbstractSlot(id + "/" + inSlot, String.class);
+		
+		IIONamespaceBuilder.initializeInSlots(slots);
+		IIONamespaceBuilder.setMasterInSlotListener(this);
+		
 		this.setID(ps.getString(ID_PROP));
-		InstantIOListener.getInstance().addInSlotListener(inslot, this);
-		System.out.println("instantIO listener listening...");
 	}
 
 	/* (non-Javadoc)
-	 * @see org.instantreality.InstantIO.InSlot.Listener#newData(org.instantreality.InstantIO.InSlot)
-	 * 
 	 * Anything received on the InstantIO network on the namespace for this object is sent through this method.
 	 * 
 	 */
 	@Override
-	public void newData(InSlot data) {
-		try {
-			String d = data.pop().toString();
-//			System.out.println(d);
-			process(d);
-		}
-		catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	@Override
-	public void startInSlot(InSlot arg0) {
-		
-	}
-
-	@Override
-	public void stopInSlot(InSlot arg0) {
-		
+	public void newData(Object arg0, Class<?> arg1, String arg2) {
+		process(arg0.toString());		
 	}
 
 }
