039d00bd223527151e5065926817e3e2e920d66a
Casey Kennington <ckennington@cit-ec.uni-bielefeld.de>
2014-10-31 16:26:04 +0100

    very minor change, removing unneeded classes

--- src/inpro/io/instantio/InstantIOInformerModule.java
+++ src/inpro/io/instantio/InstantIOInformerModule.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.instantreality.InstantIO.OutSlot;
 
+import venice.lib.AbstractSlot;
+import venice.lib.networkIIO.IIONamespaceBuilder;
+import venice.lib.networkIIO.IIONamespaceBuilder.Task;
 import edu.cmu.sphinx.util.props.PropertyException;
 import edu.cmu.sphinx.util.props.PropertySheet;
 import edu.cmu.sphinx.util.props.S4Integer;
@@ -23,24 +26,13 @@ public class InstantIOInformerModule extends IUModule {
 	static Logger log = Logger.getLogger(InstantIOInformerModule.class.getName());
 	
  	@S4String(defaultValue = "InproTK")
-	public final static String PREFIX_PROP = "prefix";	
- 	
- 	@S4Integer(defaultValue = 54445)
-	public final static String PORT_PROP = "port";
- 	
- 	@S4String(defaultValue = "127.0.0.1:54444")
-	public final static String SERVERS_PROP = "servers";
- 	
- 	@S4String(defaultValue = "namespace")
-	public final static String NAMESPACE_PROP = "namespace";
- 	
+	public final static String ID_PROP = "id";	
+
  	@S4String(defaultValue = "outslot")
 	public final static String OUTSLOT_PROP = "outslot";
+ 	
+ 	private String fullScope;
 
-	private OutSlot outSlot;
-	
- 	
- 	
 	/* (non-Javadoc)
 	 * @see inpro.incremental.IUModule#newProperties(edu.cmu.sphinx.util.props.PropertySheet)
 	 */
@@ -48,18 +40,18 @@ public class InstantIOInformerModule extends IUModule {
 	public void newProperties(PropertySheet ps) throws PropertyException {
 		super.newProperties(ps);
 		log.info("Setting up InstantIOInformerModule");
-		String prefix = ps.getString(PREFIX_PROP);
-		int port = ps.getInt(PORT_PROP);
-		String servers = ps.getString(SERVERS_PROP);
+		String id = ps.getString(ID_PROP);
 		String outslot = ps.getString(OUTSLOT_PROP);
-		String namespace = ps.getString(NAMESPACE_PROP);
-//		set up the InstantIO server
-		InstantIOInformer instance = InstantIOInformer.getInstance();
-//		instance.setPort(port);
-//		instance.setPrefix("");
-//		instance.setServers(servers);
-		instance.start();
-		outSlot = instance.addOutSlot(outslot, namespace);
+		fullScope = id +"/"+ outslot;
+		
+		IIONamespaceBuilder.setTask(Task.READ);
+		IIONamespaceBuilder.prepareNamespace("");
+		
+		AbstractSlot[] slots = new AbstractSlot[1];
+		slots[0] = new AbstractSlot(fullScope, String.class);
+		
+		IIONamespaceBuilder.initializeOutSlots(slots);
+	
 	}
 	
 	/* (non-Javadoc)
@@ -75,7 +67,7 @@ public class InstantIOInformerModule extends IUModule {
 		
 		for (EditMessage<? extends IU> edit : edits) {
 //			System.out.println("PUSHING TO INSTANTIO: " + edit);
-			outSlot.push(edit.getIU().toPayLoad());
+			IIONamespaceBuilder.write(fullScope, edit.getIU().toPayLoad(), "");
 		}
 	}
 
