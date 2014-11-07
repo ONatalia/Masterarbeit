935cd9766b19c48e92c9b66a6acf7e6800e29da8
Timo Baumann <baumann@informatik.uni-hamburg.de>
2014-11-07 15:39:22 +0100

    support for higher-sampled voices (such as recent cmu-slt-hsmm)
    there's no need for the (limited) DDS16kAudioInputStream anymore,
    this is handled in AudioUtils now and supports downsampling (would
    still fail for upsampling if there ever were a true telephone voice)

--- src/inpro/audio/AudioUtils.java
+++ src/inpro/audio/AudioUtils.java
@@ -7,10 +7,14 @@ import java.net.URL;
 
 import gov.nist.sphere.jaudio.SphereFileReader;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.spi.AudioFileReader;
+
+import marytts.util.data.audio.AudioConverterUtils;
+import marytts.util.data.audio.DDSAudioInputStream;
 
 import org.kc7bfi.jflac.sound.spi.FlacAudioFileReader;
 
@@ -35,10 +39,17 @@ public class AudioUtils {
 	}
 	
 	public static AudioInputStream get16kAudioStreamForVocodingStream(VocodingAudioStream source) {
+		DDSAudioInputStream ddsStream = new DDSAudioInputStream(source, new AudioFormat(source.getSamplingRate(), 16, 1, true, false));
 		if (source.getSamplingRate() == 16000) {
-			return new DDS16kAudioInputStream(source);
+			return ddsStream;
+		} else {
+			try {
+				return AudioConverterUtils.downSampling(ddsStream, 16000);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
 		}
-		return new DDS16kAudioInputStream(source);
 	}
 	
 }
