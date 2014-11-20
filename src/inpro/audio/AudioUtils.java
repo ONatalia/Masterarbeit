1cb167f43db95c6ef55be618c3d3a8f39c65891c
Timo Baumann <baumann@informatik.uni-hamburg.de>
2014-11-20 16:36:15 +0100

    hacky support for simple downsampling
    MaryTTS's downsampling is non-incremental, so add
    a possibility to downsample by simple averaging over
    a number of samples. This is necessary for English
    MaryTTS-5 voices which generate 48kHz audio

--- src/inpro/audio/AudioUtils.java
+++ src/inpro/audio/AudioUtils.java
@@ -13,6 +13,8 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.spi.AudioFileReader;
 
+import marytts.util.data.BaseDoubleDataSource;
+import marytts.util.data.DoubleDataSource;
 import marytts.util.data.audio.AudioConverterUtils;
 import marytts.util.data.audio.DDSAudioInputStream;
 
@@ -39,17 +41,69 @@ public class AudioUtils {
 	}
 	
 	public static AudioInputStream get16kAudioStreamForVocodingStream(VocodingAudioStream source) {
-		DDSAudioInputStream ddsStream = new DDSAudioInputStream(source, new AudioFormat(source.getSamplingRate(), 16, 1, true, false));
-		if (source.getSamplingRate() == 16000) {
-			return ddsStream;
+		if (source.getSamplingRate() == 48000) {
+			// TODO: handle 48000 incrementally by averaging or picking every second sample
+			DoubleDataSource skippingSource = new SkipStream(source, 3);
+			return new DDSAudioInputStream(skippingSource, new AudioFormat(16000, 16, 1, true, false));
+		} else if (source.getSamplingRate() == 32000) {
+			// TODO: handle 32000 incrementally by averaging or picking every second sample
+			DoubleDataSource skippingSource = new SkipStream(source, 2);
+			return new DDSAudioInputStream(skippingSource, new AudioFormat(16000, 16, 1, true, false));
 		} else {
-			try {
-				return AudioConverterUtils.downSampling(ddsStream, 16000);
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new RuntimeException(e);
+			DDSAudioInputStream ddsStream = new DDSAudioInputStream(source, new AudioFormat(source.getSamplingRate(), 16, 1, true, false));
+			if (source.getSamplingRate() == 16000) {
+				return ddsStream;
+			} else {
+				try {
+					// FIXME: add a warning that the following doesn't work incrementally!!
+					System.err.println("WARNING: downsampling is non-incremental!");
+					return AudioConverterUtils.downSampling(ddsStream, 16000);
+				} catch (Exception e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
 			}
 		}
 	}
 	
+	// this is a dirty hack:
+	private static class SkipStream extends BaseDoubleDataSource {
+		DoubleDataSource source;
+		int skip;
+		public SkipStream(VocodingAudioStream source, int skip) {
+			this.source = source;
+			this.skip = skip;
+		}
+
+	    @Override
+	    public int getData(double[] target, int targetPos, int length) {
+	    	if (available() == 0) {
+	    		try { Thread.sleep(2); // take it easy, buddy.
+				} catch (InterruptedException e) { e.printStackTrace();	}
+	    	}
+	        int outputAmount = Math.min(available() / skip, length);
+	        int inputAmount = outputAmount * skip;
+	        double[] inputValues = new double[inputAmount];
+	        source.getData(inputValues, 0, inputAmount);
+	        for (int i = 0; i < outputAmount; i++) {
+	        	target[i] = inputValues[i * skip];
+	        }
+	        return outputAmount;
+	    }
+	    
+		@Override
+		public boolean hasMoreData() {
+			return source.hasMoreData();
+		}
+		
+	    @Override
+	    public int available() {
+	        return source.available() / 2;
+	    }
+
+		@Override
+	    public long getDataLength() {
+    		return source.getDataLength() == DoubleDataSource.NOT_SPECIFIED ? DoubleDataSource.NOT_SPECIFIED : source.getDataLength() / 2; // not specified
+	    }
+	}
 }
